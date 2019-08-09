a212292c3ce85b5f71858f6fe880508d51de0e65
flysun <1290207750@qq.com>
2019-08-09 11:35:29 +0800

    genggaio

--- client/src/main/java/com/ddky/controller/HelloController.java
+++ client/src/main/java/com/ddky/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.ddky.controller;
 
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,7 +11,8 @@ public class HelloController {
 
     @Value("${server.port}")
     private int port;
-
+    @ApiParam
+    @ApiOperation(value = "接口参数", httpMethod = "GET", consumes = "name，param")
     @RequestMapping("index")
     public String index(){
         return "Hello World!,端口："+port;
